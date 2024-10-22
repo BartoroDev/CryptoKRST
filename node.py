98f745b30d477c037c1d0e70c73f308986e2bcc1
Mateusz Bartosik <mateusz.bartosik@phoenix-rtos.com>
2024-10-22 23:24:12 +0200

    Add simple tcp server

--- node.py
+++ node.py
@@ -1,49 +1,71 @@
 import argparse
 import logging
 import signal
+import socket
 from daemonize import Daemonize
 from datetime import datetime
-
 
 def parseArgs() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--init", action="store_true")
+    parser.add_argument("--join-pid", type=int)
     return parser.parse_args()
+
 
 class Node:
     def __init__(self, config: argparse.Namespace):
         self.config = config
-        self.address = "127.0.0.1"
-        self.port = 5454
+        self.server = self.prepareServer()
+        self.port = self.server.getsockname()[1]
         self.name = f"node_{self.port}"
         self.pid = f"/tmp/{self.name}.pid"
-        self.logger, fileHandler = self.prepareLogger()
-        self.log_fd = fileHandler.stream.fileno()
+        self.logger = self.prepareLogger()
 
-    def prepareLogger(self) -> tuple[logging.Logger, logging.FileHandler]:
+    def prepareServer(self) -> socket.socket:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(("localhost", 0))
+        server.listen()
+        return server
+
+    def prepareLogger(self) -> logging.Logger:
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)
         logger.propagate = False
         fh = logging.FileHandler(f"/tmp/{self.name}.log", "w")
         fh.setLevel(logging.DEBUG)
         logger.addHandler(fh)
-        return (logger, fh)
+        return logger
 
     def finish(self):
+        self.server.close()
         self.logger.warning(f"Miner {self.name} finished working at {datetime.now()}")
 
     def handleSIGTERM(self, signum, frame):
         self.finish()
 
+    def serve(self):
+        with self.server as s:
+            while True: # loop through connections
+                conn, addr = s.accept()
+                with conn:
+                    self.logger.info(f"Connected by {addr}")
+                    while True: # loop through all data
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        self.logger.info(f"received: {data}")
+                        conn.sendall(data)
+
     def start(self):
         signal.signal(signal.SIGTERM, self.handleSIGTERM)
         self.logger.info(f"Miner {self.name} started at {datetime.now()}")
         self.logger.debug(f"init config: {self.config.init}")
-        self.finish()
-
+        if self.config.init:
+            self.serve()
 
 if __name__ == "__main__":
     config = parseArgs()
     node = Node(config)
-    daemon = Daemonize(app=node.name, pid=node.pid, action=node.start, keep_fds=[node.log_fd])
+    print(f"Server listening on port: {node.port}")
+    daemon = Daemonize(app=node.name, pid=node.pid, action=node.start, auto_close_fds=False)
     daemon.start()
